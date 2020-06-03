5918e421ec79d998101b1ffbf5c1f6e342aac14b
iamayushdas <ayush24das@gmail.com>
2020-06-03 09:48:51 +0530

    port for heroku added

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const express = require('express')
 const hbs = require('hbs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 //Define path express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -120,6 +121,6 @@ app.get('*', (req, res) =>{
 //app.com/help
 //app.com/about
 
-app.listen(3000, () => {
-    console.log('server is up on port 3000')
+app.listen(port, () => {
+    console.log('server is up on port ' + port)
 })
